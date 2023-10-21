c4f81e97d86c18a943cf76d457fdd882d40de5d6
Seyed Alireza Kargar <123573604+Seyed-Alireza-Kargar@users.noreply.github.com>
2023-10-21 20:40:24 +0330

    assignment 10

---
+++ 10/3.py
@@ -0,0 +1,20 @@
+
+class Time:
+    def __init__(self,the_watch,minutes,sSeconds):
+        self.hour =the_watch
+
+        self.minutes = minutes
+
+        self.Seconds = Seconds
+
+    def add_time(self):
+        ...
+
+    def show(self):
+        ...
+
+    def Convert_seconds_to_time(self):
+        ...
+
+    def Convert_time_to_seconds(self):
+        ...    
